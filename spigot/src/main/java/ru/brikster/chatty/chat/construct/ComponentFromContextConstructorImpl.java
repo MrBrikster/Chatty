ceb76850b5071e67d92c572ea3e7c9a7b72d773c
Ilya Andreev <mrbrikster@yandex.ru>
2024-07-07 15:02:07 +0300

    Replace {player} and {message} placeholders in subcomponents

--- spigot/src/main/java/ru/brikster/chatty/chat/construct/ComponentFromContextConstructorImpl.java
+++ spigot/src/main/java/ru/brikster/chatty/chat/construct/ComponentFromContextConstructorImpl.java
@@ -1,18 +1,22 @@
 package ru.brikster.chatty.chat.construct;
 
 import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.TextReplacementConfig;
+import net.kyori.adventure.text.serializer.plain.PlainTextComponentSerializer;
 import ru.brikster.chatty.api.chat.message.context.MessageContext;
 import ru.brikster.chatty.convert.component.ComponentStringConverter;
+import ru.brikster.chatty.util.AdventureUtil;
 
 import javax.inject.Inject;
 import javax.inject.Singleton;
+import java.util.regex.Pattern;
 
 @Singleton
 public final class ComponentFromContextConstructorImpl implements ComponentFromContextConstructor {
 
     @Inject
     private ComponentStringConverter componentStringConverter;
+
+    private static final Pattern PLAYER_OR_MESSAGE_PLACEHOLDER = Pattern.compile("\\{player}|\\{message}");
 
     private static final String PLAYER_FORMAT_PLACEHOLDER = "{player}";
     private static final String MESSAGE_FORMAT_PLACEHOLDER = "{message}";
@@ -24,15 +28,24 @@ public final class ComponentFromContextConstructorImpl implements ComponentFromC
                 .replace("{original-message}", messageWithMmFormat);
         Component formattedMessageComponent = componentStringConverter.stringToComponent(formattedMessage);
 
-        return context.getFormat()
-                .replaceText(TextReplacementConfig.builder()
-                        .matchLiteral(PLAYER_FORMAT_PLACEHOLDER)
-                        .replacement(context.getSender().getDisplayName())
-                        .build())
-                .replaceText(TextReplacementConfig.builder()
-                        .matchLiteral(MESSAGE_FORMAT_PLACEHOLDER)
-                        .replacement(formattedMessageComponent)
-                        .build());
+        return AdventureUtil.replaceWithEndingSpace(context.getFormat(),
+                PLAYER_OR_MESSAGE_PLACEHOLDER,
+                matchedString -> {
+                    if (matchedString.equals(PLAYER_FORMAT_PLACEHOLDER)) {
+                        return Component.text(context.getSender().getDisplayName() + " ");
+                    } else if (matchedString.equals(MESSAGE_FORMAT_PLACEHOLDER)) {
+                        return formattedMessageComponent.append(Component.text(" "));
+                    }
+                    throw new IllegalStateException("Cannot replace player or message placeholder: " + matchedString);
+                },
+                matchedString -> {
+                    if (matchedString.equals(PLAYER_FORMAT_PLACEHOLDER)) {
+                        return context.getSender().getDisplayName() + " ";
+                    } else if (matchedString.equals(MESSAGE_FORMAT_PLACEHOLDER)) {
+                        return PlainTextComponentSerializer.plainText().serialize(formattedMessageComponent) + " ";
+                    }
+                    throw new IllegalStateException("Cannot replace player or message placeholder: " + matchedString);
+                });
     }
 
 }
